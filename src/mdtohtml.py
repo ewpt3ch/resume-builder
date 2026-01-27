236a01bd25e06eba1b158bb13c6d7b5d816b114e
Eric Phillips <eric@ewpt3ch.com>
2026-01-26 22:38:22 -0700

    use jinja2 templates to add css and formatting

--- src/mdtohtml.py
+++ src/mdtohtml.py
@@ -1,7 +1,11 @@
 from markdown_it import MarkdownIt
+from jinja2 import Environment, FileSystemLoader
 
 
 md = MarkdownIt("commonmark").enable('table')
+j2_env = Environment(loader=FileSystemLoader('templates'))
+template = j2_env.get_template('resume.template')
 
 def mdtohtml(mdsrc):
-    return md.render(mdsrc)
+    rawhtml = md.render(mdsrc)
+    return template.render(content=rawhtml)
